1123f05d62418fbf9dce17109aa33df040305767
djmartz77 <djmartz77@yahoo.com>
2016-10-13 12:28:00 -0500

    Initial brainstorming

---
+++ Design/PaintNRun.mm
@@ -0,0 +1,151 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Paint &amp; Run" FOLDED="false" ID="ID_691199104" CREATED="1476376503092" MODIFIED="1476377587477" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="2D Platformer" POSITION="right" ID="ID_122699056" CREATED="1476376513340" MODIFIED="1476377587477" HGAP_QUANTITY="9.500000134110447 pt" VSHIFT_QUANTITY="-15.749999530613437 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Payoff" ID="ID_1453090816" CREATED="1476376692452" MODIFIED="1476376817561">
+<node TEXT="End of level zoom out, showing colored in area. Score is an area" ID="ID_454446576" CREATED="1476376740237" MODIFIED="1476376863582"/>
+</node>
+<node TEXT="Look" FOLDED="true" ID="ID_1401077382" CREATED="1476376880546" MODIFIED="1476376887427">
+<node TEXT="paint drips down/trails behind player and runs down into the scene, which is like a coloring sheet/paint by numbers type deal" ID="ID_792803859" CREATED="1476376892420" MODIFIED="1476376922811"/>
+</node>
+<node TEXT="Death" ID="ID_1935288082" CREATED="1476377266945" MODIFIED="1476377272350">
+<node TEXT="when character runs out of pigment" ID="ID_1947517418" CREATED="1476377272359" MODIFIED="1476377282849"/>
+<node TEXT="falls off screen" ID="ID_1439281608" CREATED="1476377283627" MODIFIED="1476377298709"/>
+</node>
+</node>
+<node TEXT="Endless Runner" POSITION="right" ID="ID_729126671" CREATED="1476376521981" MODIFIED="1476377592036">
+<edge COLOR="#0000ff"/>
+<node TEXT="Payoff" ID="ID_3879428" CREATED="1476376692452" MODIFIED="1476377592036" HGAP_QUANTITY="20.74999979883433 pt" VSHIFT_QUANTITY="6.749999798834331 pt">
+<node TEXT="When Character dies, the perspective shifts to top down, showing the area which has been painted. Score is an area" ID="ID_1734111092" CREATED="1476376697555" MODIFIED="1476376749197"/>
+<node TEXT="When Character dies, zoom out, showing colored in area. Score is an area" ID="ID_1028722235" CREATED="1476376740237" MODIFIED="1476376791594"/>
+</node>
+<node TEXT="Look" ID="ID_1400169219" CREATED="1476376977435" MODIFIED="1476377582295" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="19.49999941885473 pt">
+<node TEXT="things the character runs over get colored in. this spreads" ID="ID_25328583" CREATED="1476376989802" MODIFIED="1476377044370"/>
+</node>
+<node TEXT="Death" ID="ID_1144559736" CREATED="1476377304623" MODIFIED="1476377570037" HGAP_QUANTITY="15.499999955296518 pt" VSHIFT_QUANTITY="20.24999939650299 pt">
+<node TEXT="Character falls of screen" ID="ID_383398619" CREATED="1476377307985" MODIFIED="1476377313081"/>
+<node TEXT="Left wall catches up to character" ID="ID_973669587" CREATED="1476377313647" MODIFIED="1476377320476"/>
+<node TEXT="Character runs out of pigment." ID="ID_1413730084" CREATED="1476377321134" MODIFIED="1476377358771"/>
+</node>
+</node>
+<node TEXT="ONLY ONE GAME MECHANIC IS ALLOWED" POSITION="left" ID="ID_342115914" CREATED="1476376539380" MODIFIED="1476376551994">
+<edge COLOR="#00ff00"/>
+<node TEXT="Mechanic 1" ID="ID_1768815486" CREATED="1476376554494" MODIFIED="1476376562016">
+<node TEXT="Swipe paint from a bucket/container in front of main character." ID="ID_1407258576" CREATED="1476376562022" MODIFIED="1476376605045"/>
+<node TEXT="If character is in contact with unpainted ground the character is depleted" ID="ID_726175051" CREATED="1476376605846" MODIFIED="1476376626544"/>
+</node>
+<node TEXT="Mechanic 2" ID="ID_475541145" CREATED="1476377069027" MODIFIED="1476377072467">
+<node TEXT="Player touches/clicks/swipes the next point that the main character will go to. Character paths to that point and picks up more paint from the blob left by the player&apos;s motion" ID="ID_693347999" CREATED="1476377073460" MODIFIED="1476377127564">
+<node TEXT="This could result in jumps as well as just running" ID="ID_74321888" CREATED="1476377130889" MODIFIED="1476377140599"/>
+</node>
+</node>
+<node TEXT="Mechanic 3" ID="ID_1115149387" CREATED="1476376554494" MODIFIED="1476377387560">
+<node TEXT="Swipe paint from a bucket/container in front of main character." ID="ID_570113684" CREATED="1476376562022" MODIFIED="1476376605045"/>
+<node TEXT="Character can&apos;t go on unpainted areas" ID="ID_751724307" CREATED="1476376605846" MODIFIED="1476377398279"/>
+</node>
+</node>
+<node TEXT="Main Character" POSITION="left" ID="ID_1949201785" CREATED="1476376631818" MODIFIED="1476376634632">
+<edge COLOR="#ff00ff"/>
+<node TEXT="1 - blob of paint" ID="ID_1763295504" CREATED="1476376635616" MODIFIED="1476376650817"/>
+<node TEXT="2 - image woken up by being colored in" ID="ID_205565374" CREATED="1476376651651" MODIFIED="1476376661736">
+<node TEXT="dripped paint on it" ID="ID_938316158" CREATED="1476376662248" MODIFIED="1476376666829"/>
+<node TEXT="????" ID="ID_213826770" CREATED="1476376667625" MODIFIED="1476376674027"/>
+</node>
+</node>
+<node TEXT="Difficulties" POSITION="right" ID="ID_568579918" CREATED="1476377157473" MODIFIED="1476377577713" HGAP_QUANTITY="13.250000022351738 pt" VSHIFT_QUANTITY="24.749999262392542 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="Areas that can&apos;t be paitned" ID="ID_1780881144" CREATED="1476377161990" MODIFIED="1476377574590" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-4.499999865889553 pt"/>
+<node TEXT="holes" ID="ID_224055330" CREATED="1476377174713" MODIFIED="1476377567336" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="6.74999979883433 pt"/>
+<node TEXT="terrain blocking the path" ID="ID_495468056" CREATED="1476377176357" MODIFIED="1476377216332"/>
+<node TEXT="Doors/gates" ID="ID_1457799891" CREATED="1476377217391" MODIFIED="1476377221256">
+<node TEXT="must color in a key or puzzle to open" ID="ID_673689922" CREATED="1476377226225" MODIFIED="1476377234731"/>
+</node>
+</node>
+<node TEXT="Intro animation" POSITION="left" ID="ID_1738705541" CREATED="1476377455215" MODIFIED="1476377460439">
+<edge COLOR="#00007c"/>
+<node TEXT="Character is filled in/filled up with color. Becomes alive/animated. Touches an uncolored area and loses the color of one foot. Now they know they can&apos;t touch uncolored things or they will die." ID="ID_167842485" CREATED="1476377460899" MODIFIED="1476377503819"/>
+</node>
+</node>
+</map>
